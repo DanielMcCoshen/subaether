5600b29ae302f0c3a91c971ee65ad54ba7236f02
Patrick Cunfer <null>
2022-01-28 23:32:47 -0600

    Initialized the model singleton

---
+++ model/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public sealed class Model {
+    private static Model instance = null;
+    private static readonly object instanceLock = new object();
+
+    private Model () {}
+
+    private static Model Instance {
+        get {
+            lock (instanceLock) {
+                if (instance == null) {
+                    instance = new Model();
+                }
+                return instance;
+            }
+        }
+    }
+}
