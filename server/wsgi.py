b0a270e1280b195e236e4d064016256f0bca328e
Daniel McCoshen <upstagecoronet@gmail.com>
2022-01-29 01:07:36 -0600

    fix wsgi.py

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,15 +8,4 @@ sys.path.insert(0, '/var/www/html/recepies/recepie_book')
 
 from server import app as application
 
-if not os.path.exists(application.config["RECEPIE_PATH"]):
-    print("Storage Directory does not exist, creating it")
-    os.makedirs(application.config["RECEPIE_PATH"])
-
-if not os.path.exists(f"{application.config['RECEPIE_PATH']}/all_recepies/"):
-    print("Createing recepie directory")
-    os.makedirs(f"{application.config['RECEPIE_PATH']}/all_recepies/")
-
-for category in application.config["CATEGORIES"]:
-    if not os.path.exists(f"{application.config['RECEPIE_PATH']}/{category}/"):
-        print(f"Createing Directory for {category}")
-        os.makedirs(f"{application.config['RECEPIE_PATH']}/{category}/")
+print("Starting Server...")
