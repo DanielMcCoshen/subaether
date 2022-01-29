7186880b1748fa28dc814c2be5ab3e7553379f4d
Patrick Cunfer <null>
2022-01-29 11:24:14 -0600

    Implement Model.Entites property

--- model/Model.cs
+++ model/Model.cs
@@ -2,11 +2,13 @@ public sealed class Model {
     private static Model instance = null;
     private static readonly object instanceLock = new object();
     private List<Entity> ownedEntities;
+    private List<Entity> externalEntities;
 
     private Model () {
         // request player Entity
         ownedEntities = new List<Entity>(/*player*/);
-        // fetch external Entity list
+        // fetch external Entity list, and assign it
+        externalEntities = new List<Entity>();
     }
 
     public static Model Instance {
@@ -17,6 +19,12 @@ public sealed class Model {
                 }
                 return instance;
             }
+        }
+    }
+
+    public List<Entity> Entities {
+        get {
+            return ownedEntities.Concat(externalEntities).ToList();
         }
     }
 }
